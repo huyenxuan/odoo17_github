0ddf2a4dea4399aa7723ba08c877c42242535ddc
xuanhuyen <goat2625@gmail.com>
2025-07-11 15:36:14 +0700

    update function action_import

--- extra_addons/mrp_extend/wizard/mrp_production_wizard.py
+++ extra_addons/mrp_extend/wizard/mrp_production_wizard.py
@@ -1,5 +1,5 @@
 from odoo import fields, models, _
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError, MissingError
 import base64
 import io
 from openpyxl import load_workbook
@@ -14,46 +14,42 @@ class MrpProductionWiz(models.TransientModel):
 
     def action_import(self):
         time_start = time.time()
+        active_id = self.env.context.get('active_id')
+        if not active_id:
+            raise MissingError(_("Manufacturing order records are not found."))
+        mo = self.env['mrp.production'].browse(active_id)
+
         if not self.file_import:
-            raise UserError(_('No file'))
-        else:
-            if not self.file_name or not self.file_name.lower().endswith(('.xls', '.xlsx')):
-                raise UserError(_('Please import file excel (.xls or .xlsx)'))
-                
-            file_data = base64.b64decode(self.file_import)
-            buffer = io.BytesIO(file_data)
-            try:
-                workbook = load_workbook(buffer, data_only=True)
-            except:
-                raise UserError(_('Không thể đọc file'))
-            
-            sheet = workbook.active
-            lines = []
+            raise FileNotFoundError(_("No file. Please import file."))
+        if not self.file_name or not self.file_name.lower().endswith(('.xls', '.xlsx')):
+            raise UserError(_("File import isn't file excel. Please import file excel."))
+        
+        file_data = io.BytesIO(base64.b64decode(self.file_import)) # Mã hóa file và đưa vào bộ nhớ đệm để xử lý
+        try:
+            workbook = load_workbook(file_data, data_only=True) # Mở file
+        except:
+            raise UnicodeDecodeError(_("Error can not read the file."))
+        
+        sheet = workbook.active
+        lines = []
 
-            # Lấy id bản ghi MO
-            active_id = self.env.context.get('active_id')
-            if not active_id:
-                raise UserError(_("No MO found"))
-            mo = self.env['mrp.production'].browse(active_id)
+        for idx, row in enumerate(sheet.iter_rows(min_row=2, values_only=True), start=2):
+            product_name, quantity = row
+            if not product_name:
+                continue
+            product = self.env['product.product'].search([
+                ('name', '=', product_name),
+            ], limit=1)
+            if not product:
+                raise UserError(_(f"Name of the product: '{product_name}' (row:{idx}) in the file is not in the product list"))
+            # Tạo mới bản ghi
+            lines.append((0, 0, {
+                'product_id': product.id,
+                'name': product_name,
+                'product_uom_qty': quantity,
+            }))
 
-            for idx, row in enumerate(sheet.iter_rows(min_row=2, values_only=True), start=2):
-                product_name, quantity = row
-                if not product_name:
-                    continue
-
-                product = self.env['product.product'].search([
-                    ('name', '=', product_name),
-                ], limit=1)
-                if not product:
-                    raise UserError(_(f"No name products have been found: '{product_name}'"))
-                
-                lines.append((0, 0, {
-                    'product_id': product.id,
-                    'name': product.name,
-                    'product_uom_qty': quantity,
-                }))
-            
-            mo.move_raw_ids = [(5, 0, 0)] + lines # Xóa xác dòng cũ và gán cái mới
+        mo.move_raw_ids = [(5, 0, 0)] + lines # Xóa toàn bộ dữ liệu cũ và ghi đè dữ liệu mới
         time_end = time.time()
         duration = time_end - time_start
         print(f'\nQuá trình import mất: {duration}s\n')
